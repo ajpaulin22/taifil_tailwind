45f1d834cb8ba25ffc80b14b402e0216263b401a
Lenard Robenta <robentalenard@gmail.com>
2023-06-28 09:41:56 +0800

    admin

--- app/Http/Controllers/MasterMaintenance/JobInformationController.php
+++ app/Http/Controllers/MasterMaintenance/JobInformationController.php
@@ -12,6 +12,25 @@ class JobInformationController extends Controller
     }
 
     public function GetJobCode(Request $request){
-
+        // GET CONNECTION ERRORS
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        
+        // SQL QUERY
+        $query = "SELECT Roll_No, Name FROM `Student Details`;";
+        // FETCHING DATA FROM DATABASE
+        $result = mysqli_query($conn, $query);
+        
+        if (mysqli_num_rows($result) > 0) {
+            // OUTPUT DATA OF EACH ROW
+            while($row = mysqli_fetch_assoc($result)) {
+                echo "Roll No: " . $row["Roll_No"]
+                . " - Name: " . $row["Name"]. "<br>";
+            }
+        } else {
+            echo "0 results";
+        }
+        $conn->close();
     }
 }
