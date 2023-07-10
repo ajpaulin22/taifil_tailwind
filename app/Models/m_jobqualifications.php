0f85420ee18b78adea06e7682d02f82536b8dbcb
Alphy Jay Paulin <aj.paulin@seiko-it.com.ph>
2023-07-10 10:05:21 +0800

    commit

---
+++ app/Models/m_jobqualifications.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class m_jobqualifications extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        "ID"
+        ,"JobCategoriesID"
+        ,"Qualification"
+        ,"IsDeleted"
+        ,"CreateID"
+        ,"UpdateID"
+        ,"updated_at"
+        ,"created_at"
+    ];
+}
