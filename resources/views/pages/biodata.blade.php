2b6e3cbfbc82347899bffcebeff46c6cced2f1fd
Alphy Jay Paulin <aj.paulin@seiko-it.com.ph>
2023-07-10 10:29:31 +0800

    change UI

--- resources/views/pages/biodata.blade.php
+++ resources/views/pages/biodata.blade.php
@@ -14,7 +14,7 @@
 
 <nav class="py-2 px-1 md:px-10 md:py-5 bg-green-500 " aria-label="Breadcrumb">
     <div class="max-w-7xl text-center md:flex justify-between mx-auto px-10">
-        <div class="text-sm md:text-2xl text-white">Qualifications</div>
+        <div class="text-sm md:text-2xl text-white">Biodata</div>
     <ol class="inline-flex items-center space-x-1 md:space-x-3">
       <li class="inline-flex items-center">
         <a href="/" class="text-white inline-flex items-center text-xs md:text-sm font-medium hover:text-blue-600">
@@ -31,7 +31,7 @@
       <li aria-current="page">
         <div class="flex items-center">
           <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-          <span class="ml-1 text-xs md:text-sm font-medium text-gray-500 md:ml-2">Qualifications</span>
+          <span class="ml-1 text-xs md:text-sm font-medium text-gray-500 md:ml-2">Biodata</span>
         </div>
       </li>
     </ol>
