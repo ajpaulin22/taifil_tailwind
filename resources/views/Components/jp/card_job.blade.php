54ffae146960e3ca41aedc8ea21721f3ed0b88af
Alphy Jay Paulin <aj.paulin@seiko-it.com.ph>
2023-07-19 11:21:05 +0800

    japanese language

--- resources/views/Components/jp/card_job.blade.php
+++ resources/views/Components/jp/card_job.blade.php
@@ -1,7 +1,7 @@
 <div class="w-full max-w-sm p-4 bg-white border border-sky-200 rounded-lg shadow sm:p-8 col-span-1 my-5 md:my-0">
     <h5 class="mb-4 text-xl font-medium text-green-500 uppercase">{{$job}}</h5>
     <div class="flex items-baseline text-gray-900 dark:text-white">
-        <span class="text-3xl font-semibold">Qualifications</span>
+        <span class="text-3xl font-semibold">資格</span>
     </div>
     <!-- List -->
     @php
@@ -16,5 +16,5 @@
         </li>
         @endforeach
     </ul>
-    <a href="/client/Biodata?data={{$type}}&type=new&cat={{$cat}}&op={{$op}}" class="text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">Apply now</a>
+    <a href="/client/Biodata?data={{$type}}&type=new&cat={{$cat}}&op={{$op}}" class="text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">今すぐ申し込む</a>
 </div>
