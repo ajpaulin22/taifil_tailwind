fb80bcc42f87a4e54e530415fae9b65ab6f0312d
Alphy Jay Paulin <aj.paulin@seiko-it.com.ph>
2023-07-27 16:00:28 +0800

    commit

--- public/js/client/Biodata.js
+++ public/js/client/Biodata.js
@@ -1958,7 +1958,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
     tw_elements.initTE({ Datepicker,Input });
     setTimeout(() => {
         $("#opening").hide();
-    }, 1000);
+    }, 300);
     // biodata.getCode();
     biodata.getData();
     biodata.getCategories()
