def4f39d9574e9f357e29083407f489d1fa1e295
Lenard Robenta <robentalenard@gmail.com>
2023-08-04 15:13:58 +0800

    08/04/2023

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <link rel="icon" type="image/png" href="/images/tf logo.png">
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
     <meta content="Tai-Fil" name="description" />
@@ -8,7 +9,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    
     <title>@yield('title') | Tai-Fil Manpower Services Corp.</title>
 
     @stack('styles')
