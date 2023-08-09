1eeee7307127a7d23e4ac493eb83a035fb3b3002
Alphy Jay Paulin <aj.paulin@seiko-it.com.ph>
2023-08-09 08:41:09 +0800

    commit

--- resources/views/jp/layout/client.blade.php
+++ resources/views/jp/layout/client.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full scroll-smooth">
+<html lang="jp" class="h-full scroll-smooth">
 <head>
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
